c23d2fd256bf663fc85b4a5350d83b033f047a91
Zosha Chung <czosha@gmail.com>
2025-02-06 11:52:19 -0500

    Task 1- Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,13 @@
+// Task 1 Creating the Customer profile 
+const customer = {
+    name: "Max Verstappen",
+    age: 22,
+    email: "maxrocks@gmail.com"
+};
+
+console.log (`Name: ${customer.name}`);
+console.log (`age: ${customer.age}`);
+console.log (`email: ${customer.email}`);
+
+// Customer Profile Created  
+
